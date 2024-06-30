1346cf9abc6fa8e6eb7e62642cd59ceb6dfc80da
Archisman Chatterjee <121617541+ArchismanChatterjee@users.noreply.github.com>
2024-07-01 01:45:13 +0530

    Update Vowels.py

--- Vowels.py
+++ Vowels.py
@@ -2,12 +2,20 @@ import math
 n= 91
 m= 45
 print (math.sqrt(n))
+# Finds out the square root of n
 print (math.sqrt(m))
+# Finds out the square root of n
 round (math.sqrt(n))
+# In case of decimals, just rounds it off. 
 print (math.sqrt(n)+ math.sqrt(m))
+# Adds the square root of two numbers
 round (math.sqrt(n)+ math.sqrt(m))
+# Rounds the addition of the square root of two numbers
 print(math.ceil(math.sqrt(n)+ math.sqrt(m)))
+# Returns the highest integer value
 print(math.floor(math.sqrt(n)+ math.sqrt(m)))
+# Returns the lowest integer value. 
 print(math.log10(math.sqrt(n)+ math.sqrt(m)))
+# Calculates base 10 logarithm of a number. 
 
 
