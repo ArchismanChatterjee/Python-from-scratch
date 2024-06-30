f997ae9c9cf50469f8e51e2a907ba3b06190f6bb
Archisman Chatterjee <121617541+ArchismanChatterjee@users.noreply.github.com>
2024-07-01 01:58:44 +0530

    Add files via upload
    Various errors of Python, discussed.

---
+++ Errors in Python.py
@@ -0,0 +1,13 @@
+# Various Errors of Python
+# There are 3 types of errors
+# Syntax errors, logical errors, and runtime errors
+lenght= 2
+breadth= 4
+perimeter= 2(lenght+breadth)# This would give a syntax error. Just insert a '*' sign. 
+
+b= 3
+c= 4
+d= 2+(b+c)
+print (d) # This is a logical error. 
+# In the third case, if you multiply a number by 0, that's going to be a Runtime error. 
+
