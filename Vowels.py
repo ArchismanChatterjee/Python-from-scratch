505b4cde6b133917ea982c5ab7e977e27fc46625
Archisman Chatterjee <121617541+ArchismanChatterjee@users.noreply.github.com>
2024-07-01 01:41:20 +0530

    Add files via upload
    Just messing around, using the various types of mathematical functions in Python.

---
+++ Vowels.py
@@ -0,0 +1,13 @@
+import math
+n= 91
+m= 45
+print (math.sqrt(n))
+print (math.sqrt(m))
+round (math.sqrt(n))
+print (math.sqrt(n)+ math.sqrt(m))
+round (math.sqrt(n)+ math.sqrt(m))
+print(math.ceil(math.sqrt(n)+ math.sqrt(m)))
+print(math.floor(math.sqrt(n)+ math.sqrt(m)))
+print(math.log10(math.sqrt(n)+ math.sqrt(m)))
+
+
